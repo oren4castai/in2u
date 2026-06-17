539a0ca62f16a049e7291ac6438e4cc8ffbe58f7
Oren Jacobi <oren.jacobi@4castai.com>
2026-06-17 08:37:45 +0300

    Delete mobile directory

--- mobile/android/app/src/main/kotlin/com/in2u/in2u/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.in2u.in2u
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- mobile/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/widgets.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'app.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ProviderScope(child: In2UApp()));
-}
